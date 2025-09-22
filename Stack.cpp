b32f4b3a4473e19d65a1e4a7f902a32db1600723
snowheartol <1049314728@qq.com>
2025-09-22 12:20:32 +0800

    完成了Stack.cpp的数组构造

---
+++ Stack.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+#define Maxsize 10
+//使用数组定义栈
+typedef struct Stack {
+	int data[Maxsize];
+	int top;
+};
+
+
+void InitStack(Stack& stack) {
+	
+	stack.top = -1;
+}
+bool isEmpty(Stack& stack) {
+	if (stack.top==-1)
+	{
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+//进栈
+bool push(Stack& stack,int value) {
+	if (stack.top==Maxsize-1)
+	{
+		return false;
+	}
+	else {
+		stack.data[++stack.top] = value;
+		return true;
+	}
+}
+//出栈
+int pop(Stack& stack) {
+	if (isEmpty(stack))
+	{
+		return 0;
+	}
+	else {
+		return stack.data[stack.top--];
+	}
+}
+int main() {
+	Stack stack;
+	InitStack(stack);
+	push(stack, 1);
+	cout<<pop(stack)<<endl;
+}
