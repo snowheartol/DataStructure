fcc6b883d5de9065d6ed36e3a86407cd7eec6c09
snowheartol <1049314728@qq.com>
2025-10-11 20:46:40 +0800

    排序函数和蓝桥杯

---
+++ 排序函数prev_permutation和next_permutation.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+//prev_permutation()和next_permutation函数的使用
+using namespace std;
+int main() {
+	vector<int> v = { 1,2,3,4 };
+	for (int num : v) {
+		cout << num;
+	}
+	cout << endl;
+
+	//生成排序
+	while (next_permutation(v.begin(), v.end())) {
+		for (int num : v) {
+			cout << num;
+		}
+		cout << endl;
+	}
+	cout << endl;
+	//返回false后会恢复成最初排列
+	for (int num : v) {
+		cout << num;
+	}
+	cout << endl;
+	//---------------------------------------------------------------
+
+
+
+	vector<int> v2 = { 4,3,2,1 };
+	for (int num : v2) {
+		cout << num;
+	}
+	cout << endl;
+	while (prev_permutation(v2.begin(), v2.end())) {
+		for (int num : v2) {
+			cout << num;
+		}
+		cout << endl;
+	}
+	cout << endl;
+	for (int num : v2) {
+		cout << num;
+	}
+
+
+	return 0;
+}
---
+++ 蓝桥杯1259.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int s = 1000000;
+	vector<int> v;
+	while (s > 0) {
+		v.push_back(s % 7);
+		s = s / 7;
+	}
+	for (int i = 0; i < v.size(); i++) {
+		if (v[i] > 5) {
+			v[i] -= 7;
+			if (i + 1 < v.size()) {
+				v[i + 1] += 1;
+			}
+			else {
+				v.push_back(1);
+			}
+		}
+	}
+	int sum = 0;
+	for (int i : v) {
+		sum += i;
+	}
+	cout << sum;
+	return 0;
+}
